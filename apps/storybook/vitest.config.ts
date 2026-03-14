2e9895851b6289dc279fae4e4492dc41ca969659
xsadia <felipe.rosa.arthur27@gmail.com>
2026-03-14 12:36:45 -0300

    feat(ci): add a11y tests with vitest addon

---
+++ apps/storybook/vitest.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from 'vitest/config';
+import { storybookTest } from '@storybook/addon-vitest/vitest-plugin';
+import { playwright } from '@vitest/browser-playwright'
+
+export default defineConfig({
+  plugins: [
+    storybookTest({
+      storybookScript: 'npm run storybook -- --ci',
+    }),
+  ],
+  test: {
+    name: 'storybook',
+    browser: {
+      enabled: true,
+      headless: true,
+      provider: playwright(),
+      instances: [{ browser: 'chromium' }],
+    },
+    setupFiles: ['.storybook/vitest.setup.ts'],
+  },
+});
